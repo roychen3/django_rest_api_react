13d1c13daecdaf3cff67401755ec4dcc93d0b15f
RoyChen <roy850323@gmail.com>
2019-12-15 17:11:52 +0800

    [6] - Auth State & Private Routes

--- leadmanager/frontend/src/actions/types.js
+++ leadmanager/frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const DELETE_LEAD = 'DELETE_LEAD';
 export const ADD_LEAD = 'ADD_LEAD';
 export const GET_ERRORS = 'GET_ERRORS';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+export const USER_LOADING = 'USER_LOADING'; // new add
+export const USER_LOADED = 'USER_LOADED'; // new add
+export const AUTH_ERROR = 'AUTH_ERROR'; // new add
